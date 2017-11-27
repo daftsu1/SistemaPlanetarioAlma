695d63ecfd60165c6db0e56a0470501c76686689
Hogarth1 <msaez.insuco3fconta@gmail.com>
2017-11-27 07:11:46 -0300

    asd

--- src/java/Model/Estrella.java
+++ src/java/Model/Estrella.java
@@ -10,8 +10,19 @@ package Model;
  * @author BuddySz
  */
 public class Estrella {
+
     private int id_estrella;
     private String nombre;
+    private int id_sistema_planetario;
+
+    public Estrella() {
+
+    }
+
+    public Estrella(int id_sistema_planetario, String nombre) {
+        this.id_estrella = id_estrella;
+        this.nombre = nombre;
+    }
 
     public int getId_estrella() {
         return id_estrella;
@@ -27,6 +38,14 @@ public class Estrella {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getId_sistema_planetario() {
+        return id_sistema_planetario;
+    }
+
+    public void setId_sistema_planetario(int id_sistema_planetario) {
+        this.id_sistema_planetario = id_sistema_planetario;
     }
 
 }
